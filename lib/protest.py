9f812592790d6940a054dedf0bf66697a9302983
V David Zvenyach <dave@esq.io>
2016-01-04 23:28:40 -0500

    cleaned the protests file

--- lib/protest.py
+++ lib/protest.py
@@ -15,21 +15,15 @@ class Protest:
         data["name"] = self.elems.xpath('//a/text()[(following::br)]')[0].strip()
         data["docket_url"] = self.elems.find_class('release_info')[0]\
                          .text_content().strip()
-        # This is currently pretty brittle because if there's no solicitation
-        # number, all of this gets thrown off, so really we need to validate
-        # the field that's identified, and then saving the attribute
+
+        # Get the field that's identified, and then save the attribute
+        # Responsible for solicitation_number, agency, file_number, and outcome
         for key in self.elems.xpath('//following-sibling::b'):
             field = key.text_content().replace(' ', '_').lower()
             data[field] = re.sub(r'^:', '', key.xpath('following-sibling::text()')[0]).strip()
 
-        # solicitation_number = self.elems.xpath('//following-sibling::b//following-sibling::text()')[0].replace(':','').strip()
-        # agency = self.elems.xpath('//following-sibling::b//following-sibling::text()')[1].replace(':','').strip()
-        # file_number = self.elems.xpath('//following-sibling::b//following-sibling::text()')[2].replace(':','').strip()
-        # outcome = self.elems.xpath('//following-sibling::b//following-sibling::text()')[3].replace(':','').strip()
-
         # TODO: For all of these dates, convert them into dates,
         # not like in current form "Dec 10, 2015"
-        # data["decided_date"] = self.elems.xpath('//following-sibling::b//following-sibling::text()')[4].replace(':','').strip()
         data["filed_date"] = self.elems.xpath('//tr/td[2]/text()')[0]
         data["due_date"] = self.elems.xpath('//tr/td[2]/text()')[1]
 
@@ -37,24 +31,11 @@ class Protest:
         data["gao_attorney"] = self.elems.xpath('//tr/td[2]/text()')[3]
 
         return data
-        # return ({
-        #     "name": name,
-        #     "url": docket_url,
-        #     "solicitation_number": solicitation_number,
-        #     "agency": agency,
-        #     "file_number": file_number,
-        #     "outcome": outcome,
-        #     "decided_date": decided_date,
-        #     "filed_date": filed_date,
-        #     "due_date": due_date,
-        #     "case_type": case_type,
-        #     "gao_attorney": gao_attorney
-        # })
 
 
 if __name__ == "__main__":
 
-    with open('tests/data/test.html', 'r') as f:
+    with open('tests/data/sustain.html', 'r') as f:
         fullhtml = f.read()
         tree = html.fromstring(fullhtml)
         protests = tree.find_class('docketSearch')
