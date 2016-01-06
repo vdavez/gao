f427272feccc2f64bd04d0bd9734ff1276ab404c
V David Zvenyach <dave@esq.io>
2016-01-05 22:01:22 -0500

    GAO generator
    Converted the get_docket_list() function in GAO to be a generator, which
    allows me to iterate over the docket's pagination. Many thanks to @paultag
    and @sbma44 for their help!

--- lib/gao.py
+++ lib/gao.py
@@ -8,34 +8,43 @@ class GAO:
         self.start_date = start_date
         self.end_date = end_date
 
+    """
+    A method to get the docket listing.
+    """
     def get_docket_list(self):
-        """
-        A method to get the docket listing.
-        """
 
-        ###
-        # Planned method of attack
-        ###
-        # Get the list of 50
-        # Process the 50 (get_protests_from_listing())
-        # Dump each of the 50 into sqlite (within get_protests_from_listing())
-        # Get next 50
-        ###
-
+        # Offset for pagination...
         offset = 0
-        url = ("http://gao.gov/legal/bid-protests/search?snumber=&filenum=&"
-               "now_sort=docdate+desc%2Cissue_date_dt+desc&page_name="
-               "bid_protest_docket&facets=a%3A3%3A%7Bs%3A4%3A%22site%22%3Bs%3A"
-               "18%3A%22Bid+Protest+Docket%22%3Bs%3A8%3A%22closed_s%22%3Bs%3A1"
-               "%3A%22Y%22%3Bs%3A7%3A%22docdate%22%3Bs%3A22%3A%22" +
-               self.start_date + "to" + self.end_date +
-               "%22%3B%7D&searched=1&rows=50"
-               "&top_path=Legal%3ABid+Protest%3ABid+Protest+Docket&path="
-               "Legal%3ABid+Protest%3ABid+Protest+Docket#bidProForm&o=" +
-               str(offset)
-               )
-        self.res = requests.get(url)
-        return self.res
+        while True:
+
+            # This should be cleaned up, but seriously facets and then "2015-01-01to2015-01-15"? Who does that?
+            url = ("http://gao.gov/legal/bid-protests/search?snumber=&filenum=&"
+                   "now_sort=docdate+desc%2Cissue_date_dt+desc&page_name="
+                   "bid_protest_docket&facets=a%3A3%3A%7Bs%3A4%3A%22site%22%3Bs%3A"
+                   "18%3A%22Bid+Protest+Docket%22%3Bs%3A8%3A%22closed_s%22%3Bs%3A1"
+                   "%3A%22Y%22%3Bs%3A7%3A%22docdate%22%3Bs%3A22%3A%22" +
+                   self.start_date + "to" + self.end_date +
+                   "%22%3B%7D&searched=1&rows=50"
+                   "&top_path=Legal%3ABid+Protest%3ABid+Protest+Docket&path="
+                   "Legal%3ABid+Protest%3ABid+Protest+Docket&o=" +
+                   str(offset) + '#bidProForm'
+                   )
+            res = requests.get(url)
+
+            # Look for the protests in the docket
+            tree = html.fromstring(res.text)
+            protests = tree.find_class('docketSearch')
+
+            # If there are no protests, stop iterating
+            if (len(protests) == 0):
+                break
+            # Otherwise, iterate
+            else:
+                # This _should_ be refactored to return the protests from the listing in the form of json...
+                yield res
+            offset += 50
+
+        return True
 
     """
     get_protests_from_listing
@@ -50,10 +59,3 @@ class GAO:
             protest = Protest(etree.tostring(p).decode('utf-8'))
             out.append(protest.data)
         return out
-
-    """
-    dump_json_into_db
-    Take the protest objects and insert them into a sqlite database
-    """
-    def dump_json_into_db(self, project):
-        pass
--- tests/test_gao.py
+++ tests/test_gao.py
@@ -11,30 +11,26 @@ class TestGAO():
         assert gao.start_date == '2015-01-01'
         assert gao.end_date == '2015-01-08'
 
-    @vcr.use_cassette('tests/data/test-initial-request.yml')
+    @vcr.use_cassette('tests/data/test-initial-request.yml', record_mode="new_episodes")
     def test_get_docket_list(self):
         gao = GAO(start_date='2015-01-01', end_date='2015-01-08')
-        res = gao.get_docket_list()
-        assert res.status_code == 200
-        assert "National Veterans Service Bureau" in res.text
+        for res in gao.get_docket_list():
+            assert res.status_code == 200
+            assert "National Veterans Service Bureau" in res.text
 
-    @vcr.use_cassette('tests/data/test-multiple-request.yml')
+    @vcr.use_cassette('tests/data/test-multiple-request.yml', record_mode="new_episodes")
     def test_get_docket_list_multiple(self):
-        gao = GAO(start_date='2015-01-01', end_date='2015-12-31')
+        gao = GAO(start_date='2015-01-01', end_date='2015-01-15')
         res = gao.get_docket_list()
-        assert res.status_code == 200
-        assert "National Veterans Service Bureau" not in res.text
+        for res in gao.get_docket_list():
+            assert res.status_code == 200
+            assert "Science Applications International Corporation" or "Man-Machine Systems Assessment Inc" in res.text
 
     @vcr.use_cassette('tests/data/test-multiple-request.yml')
     def test_get_protests_from_listing(self):
-        gao = GAO(start_date='2015-01-01', end_date='2015-12-31')
-        res_html = gao.get_docket_list()
-        res_json = gao.get_protests_from_listing(res_html.text)
-        assert len(res_json) == 50
-
-    @vcr.use_cassette('tests/data/test-multiple-request.yml')
-    def test_insert_protests_into_database(self):
-        db = sqlite3.connect(':memory:')
-        gao = GAO(start_date='2015-01-01', end_date='2015-12-31')
-        res_html = gao.get_docket_list()
-        res_json = gao.get_protests_from_listing(res_html.text)
+        gao = GAO(start_date='2015-01-01', end_date='2015-01-15')
+        res_json = []
+        for res in gao.get_docket_list():
+            res_json.append(gao.get_protests_from_listing(res.text))
+        results = [protest for dockets in res_json for protest in dockets]
+        assert len(results) == 73
